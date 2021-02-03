c8fb5ce698431912dc9e01a0b5d48f81a9b44163
johnmarsden24 <john.marsden24@gmail.com>
2021-02-03 18:10:45 +0000

    135

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -53,14 +53,6 @@ class BurgerBuilder extends Component {
   };
 
   render() {
-    // const disabledInfo = {
-    //   ...this.state.ingredients,
-    // };
-
-    // for (let key in disabledInfo) {
-    //   disabledInfo[key] = disabledInfo[key] <= 0;
-    // }
-
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
@@ -69,6 +61,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
