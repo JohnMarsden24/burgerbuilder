d854a314d8cc27e393ef5796750b745a68662162
johnmarsden24 <john.marsden24@gmail.com>
2021-02-04 15:52:51 +0000

    146

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from "react";
+
 import Aux from "../../hoc/Aux";
-import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
+import classes from "./Layout.module.css";
 
 const Layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
