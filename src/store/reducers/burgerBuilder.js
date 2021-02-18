eb45e36acb665b28bda8d875b1d844c17669ebcd
johnmarsden24 <john.marsden24@gmail.com>
2021-02-18 16:36:58 +0000

    314

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -41,6 +41,7 @@ const reducer = (state = initialState, action) => {
         ...state,
         ingredients: action.ingredients,
         error: false,
+        totalPrice: initialState.totalPrice,
       };
 
     case actionTypes.FETCH_INGREDIENTS_FAILED:
