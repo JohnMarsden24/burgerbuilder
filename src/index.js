1c2f0fb8c0903b8dcbe8d3d22bfdd78e6ef9ff84
johnmarsden24 <john.marsden24@gmail.com>
2021-02-18 15:06:04 +0000

    301

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/reducer";
 
 const store = createStore(
   reducer,
